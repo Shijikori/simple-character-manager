c292709b498213740dcc56bd983d73b6241af83f
Shijikori <jeremi.campagna@gmail.com>
2025-02-16 21:19:41 -0500

    refined Distance obj. implemented ability mod. and prof. bonus formulas

--- Program.cs
+++ Program.cs
@@ -5,8 +5,28 @@ class Distance {
     public float ConversionFactorFT = 5.0F;
     public float ConversionFactorM = 1.5F;
     
-    public Distance(int val) {
-        Value = val;
+    public Distance() {
+        Value = 0;
+        ConversionFactorFT = 5.0F;
+        ConversionFactorM = 1.5F;
+    }
+
+    public Distance(int value) {
+        Value = value;
+    }
+
+    public static Distance Parse(string distance) {
+        Distance tdist = new Distance();
+        if (distance.IndexOf("ft") > 0) {
+            tdist.SetFT((float)Int32.Parse(distance.Substring(0, distance.IndexOf("ft"))));
+        }
+        else if (distance.IndexOf("m") > 0) {
+            tdist.SetM((float)Int32.Parse(distance.Substring(0, distance.IndexOf("m"))));
+        }
+        else if (distance.IndexOf("u") > 0) {
+            tdist.Value = Int32.Parse(distance.Substring(0, distance.IndexOf("u")));
+        }
+        return tdist;
     }
 
     public void SetFT(float feetDist) {
@@ -24,31 +44,26 @@ class Distance {
     public float GetM() {
         return (float)Value * ConversionFactorM;
     }
+
+    public string Serialize() {
+        return $"{Value}u";
+    }
 }
 
-class Stat {
-    public string Name = "Strength";
-    public string SName = "STR";
-    public int Value = 8;
-    public int Modifier {
-        get {
-            int mod = 10;
-            int[] steps = {30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10, 8, 6, 4};
-            if (Value == 1) return -5;
-            for (int i = 0; i < 14; i++) {
-                if (!(Value < steps[i])) {
-                    break;
-                }
-                mod = mod - 1;
-            }
-            return mod;
+class Shape {
+    public string Type = "Sphere";
+    public Distance Size = new Distance();
+    
+    public Shape(string type, string sizeStr) {
+        Distance tSize = new Distance();
+        if (sizeStr.IndexOf("ft") > 0) {
+            tSize = Distance.Parse(sizeStr.Substring(0, sizeStr.IndexOf("ft")));
         }
-    }
-
-    public Stat(string name, string sname, int val) {
-        Name = name;
-        SName = sname;
-        Value = val;
+        else if (sizeStr.IndexOf("m") > 0) {
+            tSize.SetM((float)Int32.Parse(sizeStr.Substring(0, sizeStr.IndexOf("m"))));
+        }
+        Size = tSize;
+        Type = type;
     }
 }
 
@@ -65,31 +80,46 @@ class Character {
     public float ConversionFactorM = 1.5F;
     public string Name = "being";
     public string ClassName = "fighter";
+
+    public int ArmorClass {
+        get {
+            return 10 + GetMod(DEX);
+        }
+    }
+    public int ProficiencyBonus {
+        get {
+            return (int)Math.Ceiling((float)LVL / 4.0F) + 1;
+        }
+    }
+
+    public int GetMod(int ability) {
+        return (int)Math.Floor( ((float)ability - 10.0F) / 2.0F);
+    }
 }
 
 
+// Name, Level, Type, CastingTime, Range, Self (bool), Components (1=V,2=S,4=M), Materials[], Duration, Description
 class Spell {
-    public int DiceNum = 1;
-    public int DiceSize = 4;
-    public int AltDiceSize = 0;
-    public int PHBReferencePage = 1;
-    public int SpellLevel = 0;
-    public string Description = "a spell";
-    public string DamageType = "[any,of,list]";
-    public string SpellType = "Evocation";
-
-    public bool IsCantrip() {
-        return SpellLevel == 0;
+    public string Name = "Fireball";
+    public int Level = 1;
+    public string CastingTime = "Action";
+    public Distance Range = new Distance(0);
+    private bool _self = false;
+    public bool Self {
+        get {
+            return _self;
+        }
     }
+    public int Components = 7;
+    public List<string> Materials = new List<string>();
+    public string Duration = "Instant";
+    public string Description = "A fireball, does 1d8 damage in a Line plus 1d8 per slot level above spell level.";
 }
 
 class SCManager {
     static void Main(string[] args) {
         Console.WriteLine("Hello, World!");
-        Stat test = new Stat("Charisma", "CHA", 15);
-        Console.WriteLine(test.Modifier);
-        Distance td = new Distance(0);
-        td.SetFT(30);
+        Distance td = Distance.Parse("30ft");
         Console.WriteLine($"{td.Value}u|{td.GetM()}m|{td.GetFT()}ft");
     }
 }
