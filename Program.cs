296f5b6ef106fe7479f38e89edb584c91f5be70e
Shijikori <jeremi.campagna@gmail.com>
2025-02-18 21:27:23 -0500

    Implemented Parse() method for Shape object with corresponding serialization

--- Program.cs
+++ Program.cs
@@ -57,15 +57,37 @@ class Shape {
     public Distance Size = new Distance();
     
     public Shape(string type, string sizeStr) {
-        Distance tSize = new Distance();
-        if (sizeStr.IndexOf("ft") > 0) {
-            tSize = Distance.Parse(sizeStr.Substring(0, sizeStr.IndexOf("ft")));
-        }
-        else if (sizeStr.IndexOf("m") > 0) {
-            tSize.SetM((float)Int32.Parse(sizeStr.Substring(0, sizeStr.IndexOf("m"))));
-        }
+        Distance tSize = Distance.Parse(sizeStr);
+        
         Size = tSize;
         Type = type;
+    }
+
+    public static Shape Parse(string str) {
+        Assert(str.Length > 9); //Assertion extravaganza... '~'
+        Assert(str.IndexOf("S") == 0);
+        Assert(str.IndexOf("(") != -1 && str.IndexOf(")") != -1);
+        Assert(str.IndexOf(",") != -1);
+
+        //idk why it doesn't like "string params" but okay...
+        string paramstr = str.Substring(str.IndexOf("(") + 1, str.IndexOf(")") - (str.IndexOf("(") + 1));
+        string[] sparams = paramstr.Split(",");
+        Assert(sparams.Length == 2);
+        string ssize = "1u";
+        string stype = "";
+        foreach (string par in sparams) {
+            if (par.StartsWith("t")) {
+                stype = par.Substring(par.IndexOf(":")+1);
+            }
+            else if (par.StartsWith("s")) {
+                ssize = par.Substring(par.IndexOf(":")+1);
+            }
+        }
+        return new Shape(stype, ssize);
+    }
+
+    public string Serialize() {
+        return $"S(t:{Type},s:{Size.Serialize()})";
     }
 }
 
@@ -198,6 +220,8 @@ class SCManager {
         Console.WriteLine("Hello, World!");
         Distance td = Distance.Parse("30ft");
         Console.WriteLine($"{td.Value}u|{td.GetM()}m|{td.GetFT()}ft");
+        Shape ts = Shape.Parse("S(t:Sphere,s:5u)");
+        Console.WriteLine($"t:{ts.Type}|s:{ts.Size.Serialize()}|e:{ts.Serialize()}");
     }
 }
 
