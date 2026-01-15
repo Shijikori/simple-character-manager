d406a7af4d71d0bc20ab91d227b48f62a02456fd
Shijikori <40552956+Shijikori@users.noreply.github.com>
2026-01-14 20:52:19 -0500

    Built Main Window UI layout

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -3,4 +3,8 @@
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "SCM - Simple Character Manager";
+
+    public string? SelectedSheet { get; set; }
+
+    public string[] Sheets { get; set; } = new string[3] {"Ivar", "Confidence", "Aurelia"};
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,23 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace SCM.Views;
 
 public partial class MainWindow : Window
 {
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void ButtonHandler(object sender, RoutedEventArgs args) {
+        Button e = (Button)sender;
+        Console.WriteLine($"{e.Content} has been pressed!");
+        if (e.Name == "LoadCS") {
+            if (this.DataContext is SCM.ViewModels.MainWindowViewModel viewModel) {
+                Console.WriteLine($"{viewModel.SelectedSheet} is being loaded");
+            }
+        }
     }
 }
