1b8bd2ad5370e6fba4174feb8e67e0bc983939eb
Shijikori <40552956+Shijikori@users.noreply.github.com>
2026-01-18 18:15:02 -0500

    Added first rows of the Spellbook UI window

--- ViewModels/MasterSpellbookViewModel.cs
+++ ViewModels/MasterSpellbookViewModel.cs
@@ -1,4 +1,8 @@
+using System.Linq;
+
 namespace SCM.ViewModels;
 
 public partial class MasterSpellbookViewModel : ViewModelBase {
+    public string[] CastTimes {get; private set; } = Enum.GetNames<CastTime>();
+    public string SelectedCastTime { get; set; } = nameof(CastTime.Action);
 }
